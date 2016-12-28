8ab20285bdc49e343964e7417a5370f77bf9351a
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2016-12-28 21:15:43 +0100

    Applied drag event on windows. Though no drop yet.

--- examination/client/source/js/Window.js
+++ examination/client/source/js/Window.js
@@ -1,12 +1,12 @@
 
-let aWindow;
+let aWindow, id = 0, dragged;
 
 function newWindow(width, height, appName) {
     this.width = width;
     this.height = height;
     this.appName = appName;
 
-    aWindow = document.getElementById('window');
+    aWindow = document.getElementsByClassName('window')[0];
 
     console.log('nytt fönster');
 }
@@ -30,7 +30,23 @@ newWindow.prototype.open = function() {
         clone.appendChild(chat.chatDiv);
     }
 
+    clone.setAttribute('id', id);
+    id += 1;
+
+    this.drag(clone);
+
     aWindow.parentNode.appendChild(clone);
+};
+
+newWindow.prototype.drag = function(clone) {
+    clone.addEventListener('dragstart', function(ev) {
+        ev.dataTransfer.setData('text/plain', ev.target.id);
+        ev.dataTransfer.dropEffect = 'move';
+
+        dragged = ev.target;
+
+        console.log('drar i fönster ' + ev.target.id);
+    });
 };
 
 module.exports = newWindow;
