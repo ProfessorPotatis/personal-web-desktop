0513fa6b78ca45c21e41e672c64384d792c05365
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2016-12-29 19:10:41 +0100

    Added the possibility to open multiple windows and drag them around the desktop.

--- examination/client/source/js/Window.js
+++ examination/client/source/js/Window.js
@@ -1,14 +1,15 @@
 
-let aWindow, id = 0, dragged;
+let aWindow, id = 0;
 
 function newWindow(width, height, appName) {
     this.width = width;
     this.height = height;
     this.appName = appName;
+    this.windowPosTop = 0;
+    this.windowPosLeft = 0;
 
     aWindow = document.getElementsByClassName('window')[0];
 
-    console.log('nytt fönster');
 }
 
 newWindow.prototype.open = function() {
@@ -18,7 +19,6 @@ newWindow.prototype.open = function() {
 
         let memory = require('./Memory.js');
         let game = memory.playMemory(4, 4);
-        console.log(game.lastElementChild);
 
         clone.appendChild(game.lastElementChild);
 
@@ -33,20 +33,73 @@ newWindow.prototype.open = function() {
     clone.setAttribute('id', id);
     id += 1;
 
-    this.drag(clone);
+    this.position(clone);
 
     aWindow.parentNode.appendChild(clone);
 };
 
-newWindow.prototype.drag = function(clone) {
-    clone.addEventListener('dragstart', function(ev) {
-        ev.dataTransfer.setData('text/plain', ev.target.id);
-        ev.dataTransfer.dropEffect = 'move';
 
-        dragged = ev.target;
+newWindow.prototype.position = function(clone) {
 
-        console.log('drar i fönster ' + ev.target.id);
-    });
+    clone.style.left = this.windowPosLeft + parseInt(clone.id + 5) + 'px';
+    clone.style.top = this.windowPosTop + parseInt(clone.id + 5) + 'px';
+
+    clone.addEventListener('mousedown', function(event) {
+        this.drag(clone, event);
+    }.bind(this));
+};
+
+
+newWindow.prototype.drag = function(element, event) {
+    let startX = event.clientX, startY = event.clientY;
+    let origX = element.offsetLeft, origY = element.offsetTop;
+    let deltaX = startX - origX, deltaY = startY - origY;
+
+    if (document.addEventListener) {
+        document.addEventListener('mousemove', move, true);
+        document.addEventListener('mouseup', release, true);
+    }
+
+    if (event.stopPropagation) {
+        event.stopPropagation();
+    }
+
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+
+
+    function move(e) {
+        if (!e) {
+            e = window.event;
+        }
+
+        if (element.className === 'window') {
+            element.className += ' active';
+        }
+
+        element.style.left = (e.clientX - deltaX) + 'px';
+        element.style.top = (e.clientY - deltaY) + 'px';
+    }
+
+    function release(e) {
+        if (!e) {
+            e = window.event;
+        }
+
+        if (element.className === 'window active') {
+            element.className = 'window';
+        }
+
+        if (document.removeEventListener) {
+            document.removeEventListener('mouseup', release, true);
+            document.removeEventListener('mousemove', move, true);
+        }
+
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
+    }
 };
 
 module.exports = newWindow;
